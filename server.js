755071fa5ee16866dc05000ae99cda3d9fe6a080
Micah See <exploretheworldofdell@gmail.com>
2020-02-22 22:36:35 -0600

    get server working

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const dotenv = require('dotenv');
-const fs = require('fs');
-const path = require('path');
-if (fs.existsSync(path.join(__dirname, '/.env'))) {
-  dotenv.config();
-} else {
-  console.error('ERROR: Missing .env file, reference .env.example');
-  process.exit(1);
-}
+const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
+// if (fs.existsSync(path.join(__dirname, '/.env'))) {
+//   dotenv.config();
+// } else {
+//   console.error('ERROR: Missing .env file, reference .env.example');
+//   process.exit(1);
+// }
 
-const http = require('http');
-const app = require('./backendapp.js');
+const http = require("http");
+const app = require("./backendapp.js");
 
 const server = http.createServer(app);
-server.listen(process.env.PORT, process.env.HOST, () => {
-  console.log('Server started on ' + process.env.HOST + ':' + process.env.PORT);
+server.listen(3000, "127.0.0.1", () => {
+  console.log("Server started on " + +":" + process.env.PORT);
 });
